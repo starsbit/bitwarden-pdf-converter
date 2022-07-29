efe210b37e500a55174b17084497a777c3eb7515
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2022-07-29 18:16:14 +0200

    Add support for other dirs and compilation

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Put your bitwarden export .json file in the resources directory and rename it to
 
 Then run `bitwarden_pdf_converter.py` in the terminal.
 
+## Usage
+
+Place your bitwarden-export .json file next to the bitwarden_pdf_converter.py file.
+
+Then run `python bitwarden_pdf_converter.py` in the terminal. (requires thrid-party software, see [installation](#installation))
+
+Or run `python bitwarden_html_converter.py` in the terminal and print the file manually.
+
 ## Installation
 
 ### HTML Export
--- src/bitwarden_html_converter.py
+++ src/bitwarden_html_converter.py
@@ -2,8 +2,111 @@
     convert bitwarden export to html
 '''
 
+from glob import glob
+from os.path import exists, join
 import json
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2 import Environment
+
+
+TEMPLATE = '''<!DOCTYPE html>
+<html lang="en" class="extdovdh idc0_341">
+  <head>
+    <title>My Vault</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <!-- Latest compiled and minified CSS -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css"
+      integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu"
+      crossorigin="anonymous"
+    />
+
+    <!-- Optional theme -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css"
+      integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ"
+      crossorigin="anonymous"
+    />
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js"
+      integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <h1>My Vault</h1>
+    <section class="ftco-section">
+      <div class="container">
+        {% for category in categories %}
+        <div class="row justify-content-center">
+          <div class="col-md-6 text-center mb-5">
+            <h2 class="heading-section">{{ category.name }}</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="table-wrap">
+              <table class="table table-bordered table-dark table-hover">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Note</th>
+                    <th>Username</th>
+                    <th>Password</th>
+                    <th>URL</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {% for item in category["items"] %}
+                  <tr>
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.notes }}</td>
+                    <td>{{ item.username }}</td>
+                    <td>{{ item.password }}</td>
+                    <td>{{ item.url }}</td>
+                  </tr>
+                  {% endfor %}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        {% endfor %}
+      </div>
+    </section>
+  </body>
+</html>
+'''
+
+
+def get_password_file_name():
+    '''
+    Returns the name of the password file
+    '''
+    possibilities = ['./resources/password.json', './password.json']
+    for possibility in possibilities:
+        if exists(possibility):
+            return possibility
+
+    other_findings = []
+
+    other_findings.append(glob(join('./resources', 'bitwarden*.json')))
+    other_findings.append((glob('./bitwarden*.json')))
+
+    if len(other_findings[0]) != 0:
+        return other_findings[0][0].replace('\\', '/')
+    elif len(other_findings[1]) != 0:
+        return other_findings[1][0].replace('\\', '/')
+    else:
+        raise FileNotFoundError("No password file found")
 
 
 def check_field(bitwarden_item, field):
@@ -38,17 +141,14 @@ def parse_to_html():
     '''
     Converts bitwarden export to html
     '''
-    env = Environment(
-        loader=FileSystemLoader('src/templates'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
+    env = Environment()
 
-    template = env.get_template('template.html')
+    loaded_template = env.from_string(TEMPLATE)
     categories = []
     unrelated_category = {'name': 'Uncategorized', 'items': []}
     categories.append(unrelated_category)
 
-    with open('./resources/password.json', 'r', encoding="utf-8") as bitwarden_file:
+    with open(get_password_file_name(), 'r', encoding="utf-8") as bitwarden_file:
         data = json.load(bitwarden_file)
         for folder in data["folders"]:
             folder_id = folder["id"]
@@ -64,7 +164,7 @@ def parse_to_html():
             if item["folderId"] is None:
                 unrelated_category["items"].append(parse_item(item))
 
-    return template.render(categories=categories)
+    return loaded_template.render(categories=categories)
 
 
 with open('./resources/output.html', 'w', encoding="utf-8") as html_file:
